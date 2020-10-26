d3edc13477606f31c90d70e72b5cf9dc0132e859
tienfeek <wanges110@gmail.com>
2020-10-26 11:08:08 +0000

    use cpu norm_op in fpga device

--- lite/backends/fpga/KD/pes/norm_pe.hpp
+++ lite/backends/fpga/KD/pes/norm_pe.hpp
@@ -107,18 +107,18 @@ class NormPE : public PE {
   }
 
   bool dispatch() {
-    // cpuCompute();
+    cpuCompute();
     // std::cout << "FPGA normalize ---------------------" << std::endl;
 
-    param_.input->syncToDevice();
-    config_norm_param(norm_param_args_);
-    inplace_args_.normalize_enable = true;
-    config_inplace(inplace_args_);
+    // param_.input->syncToDevice();
+    // config_norm_param(norm_param_args_);
+    // inplace_args_.normalize_enable = true;
+    // config_inplace(inplace_args_);
 
-    perform_bypass(bypass_args_);
-    inplace_args_.normalize_enable = false;
-    config_inplace(inplace_args_);
-    compute_norm(norm_args_);
+    // perform_bypass(bypass_args_);
+    // inplace_args_.normalize_enable = false;
+    // config_inplace(inplace_args_);
+    // compute_norm(norm_args_);
 
     return true;
   }
