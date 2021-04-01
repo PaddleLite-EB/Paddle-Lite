278b0d3495fdd1c1f8cb3e8c25d4522086cb211a
root <zhulin20@baidu.com>
2021-04-01 13:16:40 +0000

    optimize single relu kernel for deeplabv3p

--- lite/backends/fpga/KD/pes/relu_pe.hpp
+++ lite/backends/fpga/KD/pes/relu_pe.hpp
@@ -30,24 +30,55 @@ class ReluPE : public PE {
     return true;
   }
 
-  void apply(FPGALock* lock = nullptr) {}
+  void apply(FPGALock* lock = nullptr) {
+    FPGALock fpga_lock(lock);
+    fpga_lock.lock();
+
+    inplace_args_.relu_enable = true;
+    inplace_args_.power_enable = false;
+    inplace_args_.normalize_enable = false;
+    
+    Shape& input_shape = param_.input->shape();
+    
+    bypass_args_.input_data_type = DATA_TYPE_FP16;
+    bypass_args_.output_data_type = DATA_TYPE_FP16;
+    bypass_args_.input_layout_type = LAYOUT_HWC;
+    bypass_args_.output_layout_type = LAYOUT_HWC;
+    bypass_args_.image.address = param_.input->data<void>();
+    bypass_args_.image.scale_address = param_.input->max();
+    bypass_args_.image.channels = input_shape.channel();
+    bypass_args_.image.height = input_shape.height();
+    bypass_args_.image.width = input_shape.width();
+    bypass_args_.output.address = param_.output->data<void>();
+    bypass_args_.output.scale_address = param_.output->max();
+  }
 
   bool dispatch(FPGALock* lock = nullptr) {
-    // FPGALock fpga_lock(lock);
-    // fpga_lock.lock();
-    param_.input->invalidate();
-    int16_t* input_data = param_.input->data<int16_t>();
-    float16* out_data = param_.output->data<float16>();
-    for (int i = 0; i < param_.input->shape().alignedElementCount(); i++) {
-      int16_t v = param_.input->data<float16>()[i];
-      if (v > 0) {
-        out_data[i] = input_data[i];
-      } else {
-        out_data[i] = zero;
-      }
-    }
-    param_.output->copyMaxFrom(param_.input);
-    param_.output->flush();
+    //cpu compute, todo
+    // param_.input->invalidate();
+    // int16_t* input_data = param_.input->data<int16_t>();
+    // float16* out_data = param_.output->data<float16>();
+    // for (int i = 0; i < param_.input->shape().alignedElementCount(); i++) {
+    //   int16_t v = param_.input->data<float16>()[i];
+    //   if (v > 0) {
+    //     out_data[i] = input_data[i];
+    //   } else {
+    //     out_data[i] = zero;
+    //   }
+    // }
+    // param_.output->copyMaxFrom(param_.input);
+    // param_.output->flush();
+
+    //fpga compute through bypass
+    FPGALock fpga_lock(lock);
+    fpga_lock.lock();
+      
+    param_.input->syncToDevice();
+    config_inplace(inplace_args_);
+    perform_bypass(bypass_args_);
+    inplace_args_.relu_enable = false;
+    config_inplace(inplace_args_);
+
     return true;
   }
 
@@ -55,6 +86,8 @@ class ReluPE : public PE {
 
  private:
   InputParam param_;
+  InplaceArgs inplace_args_ = {0};
+  BypassArgs bypass_args_;
   float16 zero = float_to_half(0.0f);
 };
 
